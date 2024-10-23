9efb7ca5b6c23198f0cf41cccb3bd512ee598322
jphulse <hulsej281@gmail.com>
2024-10-22 22:58:57 -0400

    Added columns grade_for_submission and comment_for_submission to the teams db
    Added new migration to add the teams database for grade_for_submission and comment_for_submission

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_15_192048) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_23_025333) do
   create_table "account_requests", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "username"
     t.string "full_name"
@@ -293,6 +293,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_04_15_192048) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "assignment_id", null: false
+    t.integer "grade_for_submission"
+    t.text "comment_for_submission"
     t.index ["assignment_id"], name: "index_teams_on_assignment_id"
   end
 
